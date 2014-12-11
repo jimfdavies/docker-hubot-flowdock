e412363928fbed6a426676a0aaa4410dabbb4a32
jimfdavies <jimfdavies@googlemail.com>
2014-12-11 15:32:33 +0000

    add the removal of the .git dir to obfuscate the remote

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ HUBOT_CONFIG_URL=${HUBOT_CONFIG_URL:-}
 
 git clone $HUBOT_CONFIG_URL /tmp/hubot
 
+rm -rf /tmp/hubot/.git/
 rm -rf /opt/hubot/scripts && mv /tmp/hubot/scripts/ /opt/hubot/
 rm /opt/hubot/external-scripts.json && mv /tmp/hubot/external-scripts.json /opt/hubot/
 rm /opt/hubot/package.json && mv /tmp/hubot/package.json /opt/hubot/
